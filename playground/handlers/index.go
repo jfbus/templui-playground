b889eb640c950537dcd02f256df08e92e7e4d027
jfbus <jf@bustarret.com>
2024-12-02 18:14:02 +0100

    ♻️ Renaming things

---
+++ playground/handlers/index.go
@@ -0,0 +1,12 @@
+package handlers
+
+import (
+	views "github.com/jfbus/templui-playground/playground/views"
+	"github.com/labstack/echo/v4"
+)
+
+func Index(c echo.Context) error {
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+
+	return views.Base().Render(c.Request().Context(), c.Response().Writer)
+}
